59ed110cea07b3ae994403476d212a8e133627ad
Emanuele Zeppa <zeppaemanuele@gmail.com>
2026-06-08 17:57:05 +0200

    Add files via upload

---
+++ 04 mattina/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { UserProfile } from './components/UserProfile'
+import { LoginButton } from './components/LoginButton'
+
+function App() {
+
+
+  return (
+    <>
+      <UserProfile></UserProfile>
+      <LoginButton></LoginButton>
+    </>
+  )
+}
+
+export default App
